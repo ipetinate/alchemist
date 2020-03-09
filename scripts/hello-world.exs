a90724bd117ec5c3aca0e6ff4f5cae2c7ac64803
Isac Petinate <idpetinate@gmail.com>
2020-03-09 00:27:15 -0300

    Hello World 🖖

--- scripts/hello-world.exs
+++
@@ -1,5 +0,0 @@
-IO.puts "Olá Mundo 🖖"
-IO.inspect "Hello World! 🖖 "
-
-IO.puts('Hello World! 🖖 ')
-IO.inspect("Olá Mundo! 🖖 ")
---
+++ scripts/strings.exs
@@ -0,0 +1,12 @@
+IO.puts "Olá Mundo 🖖"
+IO.inspect "Hello World! 🖖 "
+
+IO.puts('Hello World! 🖖 ')
+IO.inspect("Olá Mundo! 🖖 ")
+
+hello = "Hello"
+world = "World!"
+spock = " 🖖 "
+
+IO.inspect("Hello #{world} #{spock}")
+IO.puts("\n#{hello}\nWorld\n#{spock}")
